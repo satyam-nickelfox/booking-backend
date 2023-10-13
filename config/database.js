5eeff355131aac780a18538caa810cb182a91b70
satyam-nickelfox <satyam@nickelfox.com>
2023-10-13 10:47:15 +0530

    feat(): first commit

---
+++ config/database.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const db_url = process.env.dB_URL;
+
+// Connect to MongoDB
+mongoose.connect(db_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Event listeners for successful connection and connection error
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connected successfully!");
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+var logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const port = process.env.PORT;
+const database = require('./config/database');
+const userRouter = require('./routes/index')
+
+const app = express();
+
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', userRouter);
+
+app.listen(port, function () {
+  console.log("Server running at port : " + port);
+});
+
+module.exports = app;
